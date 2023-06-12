daf6fdc18c392f385092f4bfb7855dc792756309
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-06-12 06:21:55 +0100

    Set up dream-nav-bar

--- src/app/components/dream-catcher-components/dream-nav-bar/dream-nav-bar.component.ts
+++ src/app/components/dream-catcher-components/dream-nav-bar/dream-nav-bar.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+interface Icon {
+  name: string;
+  path: string;
+  link: string;
+}
+
 @Component({
   selector: 'app-dream-nav-bar',
   templateUrl: './dream-nav-bar.component.html',
   styleUrls: ['./dream-nav-bar.component.scss']
 })
 export class DreamNavBarComponent {
+
+  socialIcons: Icon[] = [
+    { name: 'Github', path: 'icons8-github-500.png', link: 'https://github.com/Daniel-Ope06' },
+    { name: 'LinkedIn', path: 'icons8-linkedin-500.png', link: 'https://linkedin.com/in/opeoluwa-oyedeji' },
+    { name: 'YouTube', path: 'icons8-youtube-500.png', link: 'https://www.youtube.com/@daniel_ope' },
+    { name: 'Email', path: 'icons8-mail-500.png', link: 'mailto:opeoluwaoyedeji06@gmail.com' }
+  ];
 
 }
