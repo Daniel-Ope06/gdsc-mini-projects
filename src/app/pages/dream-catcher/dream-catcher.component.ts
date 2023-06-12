4ecaa6abdcf55176ae62638d1c6318d56597e2d3
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-06-12 02:42:08 +0100

    Create dream-catcher page

---
+++ src/app/pages/dream-catcher/dream-catcher.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dream-catcher',
+  templateUrl: './dream-catcher.component.html',
+  styleUrls: ['./dream-catcher.component.scss']
+})
+export class DreamCatcherComponent {
+
+}
