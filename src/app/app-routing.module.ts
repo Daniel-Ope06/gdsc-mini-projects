343eaa33be65799d167ec505a1ae2a7ee7c2db01
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-06-12 16:53:06 +0100

    Add 'dream-catcher'  route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DreamCatcherComponent } from './pages/dream-catcher/dream-catcher.component';
+
+const routes: Routes = [
+  {path:'dream-catcher', title: 'Dream Catcher', component: DreamCatcherComponent},
+  {path:'', redirectTo: 'dream-catcher', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
