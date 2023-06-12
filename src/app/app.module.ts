a0a2fd0615b3fa4c68ea88b81cbb2f46aabeb475
Daniel-Ope22 <97707320+Daniel-Ope06@users.noreply.github.com>
2023-06-12 02:46:51 +0100

    Create dream-nav-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DreamCatcherComponent } from './pages/dream-catcher/dream-catcher.component';
+import { DreamNavBarComponent } from './components/dream-catcher-components/dream-nav-bar/dream-nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DreamCatcherComponent
+    DreamCatcherComponent,
+    DreamNavBarComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/dream-catcher-components/dream-nav-bar/dream-nav-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dream-nav-bar',
+  templateUrl: './dream-nav-bar.component.html',
+  styleUrls: ['./dream-nav-bar.component.scss']
+})
+export class DreamNavBarComponent {
+
+}
